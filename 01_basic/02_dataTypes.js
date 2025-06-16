faa93b015dd13b0e024dd2b89b0fb75a9cd7c079
Upendra Mahatara <upekhampa5@gmail.com>
2025-06-16 21:46:41 +0545

    Update the dataType.js file

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //trits all code as newer version of JS
+
+// alert(3 + 3); // we are using node.js, so no browser alert
+// code must be readable weather you use semicolon or not
+// code readability is important
+console.log(3 + 3)
+
+console.log("Upendra")
+
+// Data types in JavaScript
+let name = "Upendra" // string
+let age = 30 // number
+let isLoggedIn = true // boolean
+let state; // undefined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => stabdalone value
+// undefined => variable is not defined
+// symbol => unique value
+
+
+
+// objects
+
+console.log(typeof "name") // string
