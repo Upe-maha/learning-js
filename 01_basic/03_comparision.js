6e00708b0712980da4ce421d3b922f5b1b7ecdf4
Upendra Mahatara <upekhampa5@gmail.com>
2025-06-18 07:21:54 +0545

    Add comparision

---
+++ 01_basic/03_comparision.js
@@ -0,0 +1,33 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 === 1);
+// console.log(2 !== 1);
+
+// conform that data types are same while comparing
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+//note: comparision and equality operators are different**********
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// Equality ==
+// Strick check ===
+
+console.log("2" == 2); //true, because it converts "2" to number 2
+console.log("2" === 2);// false, because it does not convert "2" to number 2
+
+
+
+// ###### Always use clean code and readable code ######
