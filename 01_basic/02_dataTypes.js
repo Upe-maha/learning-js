01634937122e8fbbff4af06d7d2d4eb5f6a08fd2
Upendra Mahatara <upekhampa5@gmail.com>
2025-06-18 07:09:36 +0545

    Add operation

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -49,7 +49,7 @@ let idLoggedIn = ""
 
 let booleanIsLoggedIn = Boolean(idLoggedIn); // converting number to boolean
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true
 // 0 => false
@@ -58,5 +58,46 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33;
 let stringSomeNumber = String(someNumber);
-console.log(typeof stringSomeNumber);
-console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+
+
+//******** Operation *****************
+
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2);
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let str1 = "Hello";
+let str2 = " Mahatara";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3; //not recommended to declare multiple variables in one line
+num1 = num2 = num3 = 2 + 2;
+
+// Prefix and Postfix Increment/Decrement Operators
+let gameCounter = 100;
+// ++gameCounter;
+console.log(gameCounter);
+const gamePlayer = gameCounter++;
+console.log(gamePlayer, gameCounter);
+
