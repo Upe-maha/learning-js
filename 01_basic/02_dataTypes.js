a7efcd63a684c73ce8bcb1cb209f6ab62da322ae
Upendra Mahatara <upekhampa5@gmail.com>
2025-06-18 06:40:26 +0545

    Add )2_dataType file

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -3,9 +3,9 @@
 // alert(3 + 3); // we are using node.js, so no browser alert
 // code must be readable weather you use semicolon or not
 // code readability is important
-console.log(3 + 3)
+// console.log(3 + 3)
 
-console.log("Upendra")
+// console.log("Upendra")
 
 // Data types in JavaScript
 let name = "Upendra" // string
@@ -25,4 +25,38 @@ let state; // undefined
 
 // objects
 
-console.log(typeof "name") // string
+// console.log(typeof "name") // string
+// console.log(typeof age) // number
+
+// let score = "Upendra";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score); // converting string to number
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber); //NaN represents Not a Number in output
+
+//Notes
+// "33" => 33
+// "33abc" => NaN
+//true => 1
+//false => 0
+// null => 0
+// undefined => NaN
+
+let idLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(idLoggedIn); // converting number to boolean
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Upendra" => true
+
+let someNumber = 33;
+let stringSomeNumber = String(someNumber);
+console.log(typeof stringSomeNumber);
+console.log(stringSomeNumber);
