fd42213debad5da771bb76cad1c4b8d7f496518d
Upendra Mahatara <upekhampa5@gmail.com>
2025-06-22 06:07:52 +0545

    Add lesson related to Date and Time

---
+++ 01_basic/06_dates.js
@@ -0,0 +1,36 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+
+//TimeStamp
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);   //Results are alwaya in milisecond
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now() / 1000)); /// resust are in seconds now.
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+
+
+
+
+
